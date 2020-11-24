d8ab93d086d2c59772e131984651b033641f25d4
Marcus Luk <marcus.luk@oocl.com>
2020-11-24 18:34:51 +0800

    [PosMachine][Marcus Luk] feat: Finished task

---
+++ PDCA.md
@@ -0,0 +1,24 @@
+# PDCA: Pos machine
+## printReceipt
+P:	15 minutes  
+D: 	20 minutes  
+C:	Not familiar with stream, spend a lot of time to handle the barcodes  
+A:	Practice more with stream  
+
+## countItem
+P:	1 minutes  
+D: 	1 minutes  
+C:	/  
+A:	/  
+
+## findItemByBarcode
+P:	3 minutes  
+D:	3 minutes  
+C:	/  
+A:	/  
+
+## createLine
+P:	1 minutes  
+D:	1 minutes  
+C:	/  
+A:	/  
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,7 +3,33 @@ package pos.machine;
 import java.util.List;
 
 public class PosMachine {
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+        int total = 0;
+        String receipt = "***<store earning no money>Receipt***\n";
+        String[] distinctBarcodes = barcodes.stream().distinct().toArray(String[]::new);
+
+        for(String barcode: distinctBarcodes){
+            ItemInfo itemInfo = findItemByBarcode(barcode);
+            int amount = countItem(barcodes, barcode);
+            receipt += createLine(itemInfo, amount);
+            total += itemInfo.getPrice() * amount;
+        }
+
+        receipt += "----------------------\n" + "Total: " + total + " (yuan)\n" + "**********************";
+
+        return receipt;
+    }
+
+    private int countItem(List<String> barcodes, String targetBarcode) {
+        return barcodes.stream().filter(o -> o.equals(targetBarcode)).toArray().length;
+    }
+
+    private ItemInfo findItemByBarcode(String barcode){
+        return ItemDataLoader.loadAllItemInfos().stream().filter(item -> item.getBarcode().equals(barcode)).findFirst().orElse(null);
+    }
+
+    private String createLine(ItemInfo itemInfo, int amount){
+        return "Name: " + itemInfo.getName() + ", Quantity: " + amount + ", Unit price: " + itemInfo.getPrice() + " (yuan), Subtotal: " + amount*itemInfo.getPrice() + " (yuan)\n";
     }
 }
