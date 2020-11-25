b65654dd7a026be554f7a439883bc50d8273fbd4
Marcus Luk <marcus.luk@oocl.com>
2020-11-25 10:35:06 +0800

    [PosMachine][Marcus Luk] feat: Take away database access from for loop. Use string builder for receipt creation.

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -6,30 +6,33 @@ public class PosMachine {
 
     public String printReceipt(List<String> barcodes) {
         int total = 0;
-        String receipt = "***<store earning no money>Receipt***\n";
-        String[] distinctBarcodes = barcodes.stream().distinct().toArray(String[]::new);
+        StringBuilder receipt = new StringBuilder("***<store earning no money>Receipt***\n");
+        List<ItemInfo> allItemInfo = ItemDataLoader.loadAllItemInfos();
 
-        for(String barcode: distinctBarcodes){
-            ItemInfo itemInfo = findItemByBarcode(barcode);
+        String[] distinctBarcodes = barcodes.stream().distinct().toArray(String[]::new);
+        for (String barcode : distinctBarcodes) {
+            ItemInfo itemInfo = findItemByBarcode(allItemInfo, barcode);
             int amount = countItem(barcodes, barcode);
-            receipt += createLine(itemInfo, amount);
-            total += itemInfo.getPrice() * amount;
+            receipt.append(createLine(itemInfo, amount));       // concat the receipt
+            total += itemInfo.getPrice() * amount;              // accumulate the total
         }
 
-        receipt += "----------------------\n" + "Total: " + total + " (yuan)\n" + "**********************";
+        receipt.append("----------------------\nTotal: ")
+                .append(total)
+                .append(" (yuan)\n**********************");
 
-        return receipt;
+        return receipt.toString();
     }
 
     private int countItem(List<String> barcodes, String targetBarcode) {
         return barcodes.stream().filter(o -> o.equals(targetBarcode)).toArray().length;
     }
 
-    private ItemInfo findItemByBarcode(String barcode){
-        return ItemDataLoader.loadAllItemInfos().stream().filter(item -> item.getBarcode().equals(barcode)).findFirst().orElse(null);
+    private ItemInfo findItemByBarcode(List<ItemInfo> allItemInfo, String barcode) {
+        return allItemInfo.stream().filter(item -> item.getBarcode().equals(barcode)).findFirst().orElse(null);
     }
 
-    private String createLine(ItemInfo itemInfo, int amount){
-        return "Name: " + itemInfo.getName() + ", Quantity: " + amount + ", Unit price: " + itemInfo.getPrice() + " (yuan), Subtotal: " + amount*itemInfo.getPrice() + " (yuan)\n";
+    private String createLine(ItemInfo itemInfo, int amount) {
+        return "Name: " + itemInfo.getName() + ", Quantity: " + amount + ", Unit price: " + itemInfo.getPrice() + " (yuan), Subtotal: " + amount * itemInfo.getPrice() + " (yuan)\n";
     }
 }
